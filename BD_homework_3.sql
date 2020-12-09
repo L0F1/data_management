4c99e4376cb346a17de1691830d228c0589fa18c
sotcode <62123901+sotcode@users.noreply.github.com>
2020-12-09 23:30:02 +0300

    Update BD_homework_3.sql

--- BD_homework_3.sql
+++ BD_homework_3.sql
@@ -1,9 +1,9 @@
 SELECT 'ФИО: Сотников Иван Дмитриевич';
 
 SELECT userid, movieid,
-((rating - MIN(rating) OVER (PARTITION BY userid))
-/ (MAX(rating) OVER (PARTITION BY userid)
-- MIN(rating) OVER (PARTITION BY userid))) as norm_rating,
+((rating - min(rating) OVER (PARTITION BY userid))
+/ (max(rating) OVER (PARTITION BY userid)
+- min(rating) OVER (PARTITION BY userid))) as norm_rating,
 avg(rating) OVER (PARTITION BY userid) AS avg_rating
 FROM movie.ratings
 ORDER BY userid
