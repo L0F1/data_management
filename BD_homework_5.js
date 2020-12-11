6a199336f8deb59f3c0fb73928235019f105e9a3
sotcode <62123901+sotcode@users.noreply.github.com>
2020-12-11 19:09:10 +0300

    Add files via upload

---
+++ BD_homework_5.js
@@ -0,0 +1,40 @@
+FIO = 'Сотников Иван Дмитриевич'
+
+count = db.tags.find().count()
+
+print('tags count: ', count);
+// output: tags count: 91106
+
+count = db.tags.find({"tag_name": "Adventure"}).count()
+
+print('Adventure tags count: ', count);
+// output: Adventure tags count: 3496
+
+printjson(
+        db.tags.aggregate([
+                {$group: {
+                                "_id":"$tag_name",
+                                "count": {$sum: 1}
+                           }
+                },
+                {$sort: {count: -1}},
+                {$limit: 3}
+        ])['_batch']
+);
+
+/* output:
+[
+	{
+		"_id" : "Thriller",
+		"count" : 7624
+	},
+	{
+		"_id" : "Comedy",
+		"count" : 13182
+	},
+	{
+		"_id" : "Drama",
+		"count" : 20265
+	}
+]
+*/
