0d647a86ebf6b3e9b8b1c4935107d944a661b90f
sotcode <62123901+sotcode@users.noreply.github.com>
2020-12-29 16:13:07 +0300

    Update BD_homework_1.sql

--- BD_homework_1.sql
+++ BD_homework_1.sql
@@ -1,3 +1,4 @@
+<pre>
 SELECT 'ФИО: Сотников Иван Дмитриевич';
 
 CREATE SCHEMA IF NOT EXISTS P2C;
@@ -37,3 +38,4 @@ INSERT INTO P2C.persons2content VALUES (1, 2, 'producer');
 INSERT INTO P2C.persons2content VALUES (2, 3, 'producer');
 INSERT INTO P2C.persons2content VALUES (3, 4, 'producer');
 INSERT INTO P2C.persons2content VALUES (4, 5, 'producer');
+</pre>
